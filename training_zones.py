caf3ec5d48314b4d5023920cbd4d6a0ae9cf3cab
callum1864267 <a1864267@adelaide.edu.au>
2024-08-20 13:21:31 +0930

    Update training_zones.py

--- training_zones.py
+++ training_zones.py
@@ -1,6 +1,23 @@
 import pandas as pd
 
 def get_training_data():
+    zones_HR = []
+    maxHR = 0
+    restHR = 0
+    print("Enter ")
+
+    for zone in zones:
+        while True:
+            try:
+                time = float(input(f"{zone}: "))
+                times.append(time)
+                break
+            except ValueError:
+                print("Please enter a valid number.")
+
+    return zones, times
+
+def get_training_data():
     zones = ["UT3", "UT2", "UT1", "AT", "TR", "AC", "AP"]
     times = []
 
